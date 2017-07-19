9c4c4c3c4e17411f53e7b1c2489ec8e13e5a9408
alipiapia <124910168@qq.com>
2017-07-19 22:18:43 +0800

    PM. 19th July,2017

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -58,7 +58,7 @@ class Order extends Home
         if(!is_mobile()){
             return "提示：请使用手机访问！";
         }
-        pp(input(''));
+        // pp(input(''));
         $data = request()->get();
         if(empty($data['product_id'])){
             $this->error("商品不存在");
